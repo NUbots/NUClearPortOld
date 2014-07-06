5e435cd155fea579f3b0c8a6c5f77e7b6ef7aa40
Trent Houliston <trent@houliston.me>
2014-07-06 16:29:49 +1000

    Things related to obstacle detection

--- modules/vision/ObstacleDetector/src/ObstacleDetector.cpp
+++ modules/vision/ObstacleDetector/src/ObstacleDetector.cpp
@@ -33,7 +33,13 @@ namespace vision {
 
         on<Trigger<ClassifiedImage<ObjectClass>>, Options<Single>>("Obstacle Detector", [this](const ClassifiedImage<ObjectClass>& image) {
 
-            std::vector<arma::ivec2> points;
+            struct PointData {
+                arma::ivec2 point;
+                int direction;
+                ObjectClass team;
+            };
+
+            std::vector<PointData> points;
 
             // Get all the segments that are relevant to finding an obstacle
             for(int i = 0; i < 6; ++i) {
@@ -43,33 +49,58 @@ namespace vision {
                               : i == 2 ? image.horizontalSegments.equal_range(ObjectClass::CYAN_TEAM)
                               : i == 3 ? image.verticalSegments.equal_range(ObjectClass::CYAN_TEAM)
                               : i == 4 ? image.horizontalSegments.equal_range(ObjectClass::MAGENTA_TEAM)
-                              : image.verticalSegments.equal_range(ObjectClass::MAGENTA_TEAM);
+                              :          image.verticalSegments.equal_range(ObjectClass::MAGENTA_TEAM);
+
+                auto colour = i == 0 ? ObjectClass::UNKNOWN
+                            : i == 1 ? ObjectClass::UNKNOWN
+                            : i == 2 ? ObjectClass::CYAN_TEAM
+                            : i == 3 ? ObjectClass::CYAN_TEAM
+                            : i == 4 ? ObjectClass::MAGENTA_TEAM
+                            :          ObjectClass::MAGENTA_TEAM;
 
                 for(auto it = segments.first; it != segments.second; ++it) {
 
                     auto& start = it->second.start;
                     auto& end = it->second.end;
 
-                    // Check if we have a subsequent segment
-                    if(it->second.previous && image.visualHorizonAtPoint(start[0]) < start[1]) {
-                        points.push_back(start);
+                    // Push back starts only if we are a horizontal segment
+                    if(it->second.previous && image.visualHorizonAtPoint(start[0]) < start[1] && !(i % 2)) {
+                        points.push_back({ start, 1, colour });
                     }
 
                     // Check if we have a subsequent segment
                     if(it->second.next && image.visualHorizonAtPoint(end[0]) < end[1]) {
-                        points.push_back(end);
+                        points.push_back({ end, !(i % 2) ? -1 : 0, colour });
                     }
                 }
             }
 
-            // Sort our points
-            std::sort(points.begin(), points.end(), [] (const arma::ivec2& a, const arma::ivec2& b) {
-                return a[0] < b[0];
+            // Sort our points by their x position
+            std::sort(points.begin(), points.end(), [] (const PointData& a, const PointData& b) {
+                return a.point[0] < b.point[0];
             });
 
-            // Now build a locally convex hull or something
+            int counter = 0;
+            std::vector<PointData>::iterator start;
+            std::vector<PointData>::iterator end;
+            for(auto it = points.begin(); it != points.end(); ++it) {
+                // We add to the counter what our transition is
+                counter += it->direction;
 
-            // Or possibly do a threshold check (for every start there is an end, if all segments end we have reached the end of the obstacle)
+                if(counter == MINIMUM_SEGMENTS_FOR_OBSTACLE && points->direction == 1) {
+                    // Start tracking our segments
+                    start = it;
+                }
+                else if(counter == MINIMUM_SEGMENTS_FOR_OBSTACLE && points->direction == -1) {
+                    // Build our obstacle by doing a hull of some sort from the highest point
+                }
+
+                // If we are at the boundry and we just added one
+                // Start appending points
+
+                // if we are at the boundry and we just subtracted one
+                // Build the obstacle
+            }
 
             //JAKE's Vision Kinematics for distance to obstacle:
             // arma::vec2 p1 = imageToScreen(obstacleBaseCentreImage, { double(image.dimensions[0]), double(image.dimensions[1]) });
